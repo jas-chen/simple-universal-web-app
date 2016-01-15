918af1c98e993a5782cbd3a6fc2409ba800c7320
Jas Chen <jas228@gmail.com>
2016-01-15 21:14:40 +0800

    Make server use React

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import React from 'react';
+import ReactServer from 'react-dom/server';
+import App from 'shared/view/App';
 
 const app = express();
 const NODE_PORT = process.env.NODE_PORT || 3000;
 const NODE_HOST = process.env.NODE_HOST || '0.0.0.0';
 
+function renderHtml(element) {
+  const app = ReactServer.renderToString(element);
+
+  return `<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello</title>
+  </head>
+  <body>
+    <div id="app">${app}</div>
+  </body>
+</html>`;
+}
+
 app.get('/', (req, res) => {
-  res.send('ok');
+  res.send(renderHtml(<App />));
 });
 
 app.listen(NODE_PORT, NODE_HOST, (err) => {
